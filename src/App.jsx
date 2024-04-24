837539b21e0b37d83641f7d7e5d6ae4b09cf6233
Sonualam <sonualam1599@gmail.com>
2024-04-24 18:10:12 +0530

    starter template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
+      <h1>The Weather App</h1>
+    </div>
+  );
+}
+
+export default App;
